d979950d809cf5ec4f2214ab86c54d30453eceb4
Mathis Santos <mail@mathissantos.com>
2023-12-12 09:46:12 +0100

    corrected index, CharacterCard js

--- components/CharacterCard/CharacterCard.js
+++ components/CharacterCard/CharacterCard.js
@@ -1,27 +1,25 @@
-import { cardContainer } from "../../index.js";
-
 export function CharacterCard(props) {
   const card = document.createElement("li");
-  card.classList.add("card");
+  const cardContainer = document.querySelector('[data-js="card-container"]');
   card.innerHTML = `<div class="card__image-container">
-    <img
-      class="card__image"
-      src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-      alt="Rick Sanchez"
-      data-js="card-image"/>
+     <img
+       class="card__image"
+       src="${props.image}
+       alt="Image of the character ${props.name}"
+       />
     <div class="card__image-gradient"></div>
-  </div>
-  <div class="card__content">
-    <h2 class="card__title" data-js="card-name">${props.name}</h2>
-    <dl class="card__info">
-      <dt class="card__info-title" data-js="card-status">Status</dt>
-      <dd class="card__info-description">${props.status}</dd>
-      <dt class="card__info-title" data-js="card-type">Type</dt>
-      <dd class="card__info-description">${props.type}</dd>
-      <dt class="card__info-title" data-js="card-occurrences">Occurrences</dt>
-      <dd class="card__info-description">51</dd>
-    </dl>
-  </div>`;
+   </div>
+   <div class="card__content">
+   <h2 class="card__title">${props.name}</h2>
+     <dl class="card__info">
+       <dt class="card__info-title">Status</dt>
+       <dd class="card__info-description">${props.status}</dd>
+       <dt class="card__info-title">Type</dt>
+       <dd class="card__info-description">${props.type}</dd>
+       <dt class="card__info-title">Occurrences</dt>
+       <dd class="card__info-description">${props.length}</dd>
+     </dl>
+   </div>`;
   cardContainer.append(card);
   //   return card;
 }
