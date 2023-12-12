310211c757ca711221709dead41ad638231e9a8a
Mathis Santos <mail@mathissantos.com>
2023-12-12 10:37:03 +0100

    optimized fetchCharacter, crossorigin added to img src

--- components/CharacterCard/CharacterCard.js
+++ components/CharacterCard/CharacterCard.js
@@ -1,10 +1,11 @@
 export function CharacterCard(props) {
   const card = document.createElement("li");
-  const cardContainer = document.querySelector('[data-js="card-container"]');
-  card.innerHTML = `<div class="card__image-container">
+  card.innerHTML = `
+  <div class="card__image-container">
      <img
        class="card__image"
-       src="${props.image}
+       crossorigin
+       src=${props.image}
        alt="Image of the character ${props.name}"
        />
     <div class="card__image-gradient"></div>
@@ -17,9 +18,9 @@ export function CharacterCard(props) {
        <dt class="card__info-title">Type</dt>
        <dd class="card__info-description">${props.type}</dd>
        <dt class="card__info-title">Occurrences</dt>
-       <dd class="card__info-description">${props.length}</dd>
+       <dd class="card__info-description">${props.episode.length}</dd>
      </dl>
    </div>`;
-  cardContainer.append(card);
-  //   return card;
+  console.log(props.episode.length);
+  return card;
 }
